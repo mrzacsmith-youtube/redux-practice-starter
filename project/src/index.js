8f41b26d9280dad197059a99ea41f5c8b67b0078
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-06-11 17:25:53 -0500

    createStore and Provider

--- project/src/index.js
+++ project/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import thunk from "redux-thunk";
 
+const store = CreateStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
